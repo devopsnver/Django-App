608e16b4963721ddd6cbd269f76217d7f6515ea5
devopsnver <126852245+devopsnver@users.noreply.github.com>
2024-04-21 15:15:35 +0000

    uwsgi polls module not found error last step

--- app/polls/polls/settings.py
+++ app/polls/polls/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default='default-secret-key-for-building')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': config("POSTGRES_DB"),
         'USER': config("POSTGRES_USER"),
         'PASSWORD': config("POSTGRES_PASSWORD"),
-        'HOST': 'db',
+        'HOST': 'postgres',
         'PORT': 5432,
     }
 }
