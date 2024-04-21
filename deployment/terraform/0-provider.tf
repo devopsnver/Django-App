b98991d78aa16760770c4fdc7bc9f56aeaf69cc3
devopsnver <126852245+devopsnver@users.noreply.github.com>
2024-04-21 11:59:12 +0000

    update files eks working

---
+++ deployment/terraform/0-provider.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
